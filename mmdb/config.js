72ffe43acd475d9e7b0c4bfa37ce550ba4f9faf9
cryptolokimax <cryptolokimax@gmail.com>
2021-01-10 12:12:32 +0300

    Add ASN data

--- mmdb/config.js
+++ mmdb/config.js
@@ -9,4 +9,8 @@ exports.geoIpDbs = [
     filename: 'GeoLite2-City',
     remote: `https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=${process.env.MAXMIND_LICENSE_KEY}&suffix=tar.gz`,
   },
+  {
+    filename: 'GeoLite2-ASN',
+    remote: `https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-ASN&license_key=${process.env.MAXMIND_LICENSE_KEY}&suffix=tar.gz`,
+  },
 ]
