65f8551d041c8d1dc24748a7f2fc2113e4af550f
tomobre <tomobre@gmail.com>
2023-05-10 16:16:59 +1000

    test

--- mmdb/config.js
+++ mmdb/config.js
@@ -4,13 +4,13 @@
 exports.dbDir = __dirname
 
 // local:filename, remote:geolite-url
-exports.geoIpDbs = [
+exports.geoIpDbs = (licenseKey) => [
   {
     filename: 'GeoLite2-City',
-    remote: `https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=${process.env.MAXMIND_LICENSE_KEY}&suffix=tar.gz`,
+    remote: `https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=${licenseKey}&suffix=tar.gz`,
   },
   {
     filename: 'GeoLite2-ASN',
-    remote: `https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-ASN&license_key=${process.env.MAXMIND_LICENSE_KEY}&suffix=tar.gz`,
+    remote: `https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-ASN&license_key=${licenseKey}&suffix=tar.gz`,
   },
 ]
